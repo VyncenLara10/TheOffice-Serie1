1f8e7be0728348a18983c601584913d5bd375f1b
Vyncen Lara <vyncen.lara@gmail.com>
2023-02-14 09:48:01 -0600

    Subi el archivo .py
    Agregue un archivo

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 #Datos
 print "Vyncen Lara de león carnet: 1615823 "
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
+
+
+
